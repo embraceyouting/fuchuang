9a4661eb872920871a37ed160e5dcd9059b52e41
TuGitee <tgb@std.uestc.edu.cn>
2024-03-18 17:19:06 +0800

    fix: animate

--- serve/src/directives/animate.js
+++ serve/src/directives/animate.js
@@ -6,19 +6,7 @@ export default {
 		};
 
 		const direction = binding.value?.direction || "left";
-		const offset = binding.value?.offset || 200;
 		const duration = binding.value?.duration || "0.6s";
-		const transform = `translate${
-			direction.toUpperCase() === "LEFT" ||
-			direction.toUpperCase() === "RIGHT"
-				? "X"
-				: "Y"
-		}(${
-			direction.toUpperCase() === "LEFT" ||
-			direction.toUpperCase() === "TOP"
-				? "-"
-				: "+"
-		}${offset}px)`;
 
 		let observer = new IntersectionObserver((entries, observer) => {
 			entries.forEach((entry) => {
@@ -27,17 +15,30 @@ export default {
 					el.style.transform = `translate(0)`;
 					el.style.opacity = 1;
 				} else {
-					init()
+					init();
 				}
 			});
 		}, options);
 
 		function init() {
-			el.style.transform = transform;
+			el.style.transform = `translate${
+				direction.toUpperCase() === "LEFT" ||
+				direction.toUpperCase() === "RIGHT"
+					? "X"
+					: "Y"
+			}(${
+				direction.toUpperCase() === "LEFT" ||
+				direction.toUpperCase() === "TOP"
+					? "-"
+					: "+"
+			}${
+				binding.value?.offset ||
+				(window.innerWidth - el.getBoundingClientRect().width) / 2 - 20
+			}px)`;
 			el.style.opacity = 0;
 			observer.observe(el);
 		}
 
-        init()
+		init();
 	},
 };
