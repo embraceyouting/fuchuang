c76baf59a5b0b03e59687f2ebff7aa333c6a6ea0
TuGitee <tgb@std.uestc.edu.cn>
2024-03-31 22:13:59 +0800

    feat: delete cover route

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,13 +3,11 @@ const router = express.Router();
 
 const baseRoute = require("./base.js");
 const gptRoute = require("./gpt.js");
-const coverRoute = require("./cover.js");
 const userRoute = require("./user.js");
 const subjectRoute = require("./subject.js");
 
 router.use("/", baseRoute);
 router.use("/gpt", gptRoute);
-router.use("/cover", coverRoute);
 router.use("/user", userRoute);
 router.use("/subject", subjectRoute);
 
