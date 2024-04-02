d5444a620a28948f9a214231cc9c21c022df5de0
TuGitee <tgb@std.uestc.edu.cn>
2024-04-02 14:12:10 +0800

    fix: logo icon

--- serve/src/icons/Logo.vue
+++ serve/src/icons/Logo.vue
@@ -6,11 +6,10 @@
 
 <style>
 .icon {
-    width: 1.8em;
-    height: 1.8em;
+    width: 16px;
+    height: 16px;
+    transform: scale(1.5);
     vertical-align: -0.15em;
-    fill: linear-gradient(to right, #ffffff, #0000ff) !important;
-    /* 使用 fill 属性 */
     overflow: hidden;
 }
 </style>
