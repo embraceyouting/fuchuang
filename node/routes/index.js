b531a20dff79df71e07ed9043b015b5b3601ed29
embraceyouting <embraceyou30@gmail.com>
2024-04-09 16:35:45 +0800

    select

--- node/routes/index.js
+++ node/routes/index.js
@@ -5,7 +5,6 @@ const baseRoute = require("./base.js");
 const gptRoute = require("./gpt.js");
 const userRoute = require("./user.js");
 const subjectRoute = require("./subject.js");
-const visualRoute = require("./visual.js");
 
 router.use("/", baseRoute);
 router.use("/gpt", gptRoute);
