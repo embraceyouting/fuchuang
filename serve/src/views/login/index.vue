6267bb9d8afc01f08a9d7bd287f1ba14663c017e
TuGitee <tgb@std.uestc.edu.cn>
2024-04-20 01:56:06 +0800

    fix: login page

--- serve/src/views/login/index.vue
+++ serve/src/views/login/index.vue
@@ -3,9 +3,11 @@
         <div class="modal-container">
             <div class="canvas">
                 <img class="bihua" src="@/assets/image/bihua.png" alt="">
-                <img class="fei" src="@/assets/image/fei.png" v-animate="{ direction: 'bottom', offset: 40, duration: '1s' }" alt="">
-                <img class="yue" src="@/assets/image/yue.png" v-animate="{ direction: 'top', offset: 40, duration: '1s' }" alt="">
-                <canvas ref="canvas"></canvas>
+                <img class="fei" src="@/assets/image/fei.png"
+                    v-animate="{ direction: 'bottom', offset: 40, duration: '1s' }" alt="">
+                <img class="yue" src="@/assets/image/yue.png"
+                    v-animate="{ direction: 'top', offset: 40, duration: '1s' }" alt="">
+                <canvas ref="canvas" v-if="!isMobile"></canvas>
             </div>
             <div class="middle_part">
                 <Login></Login>
@@ -18,9 +20,14 @@
 import { onMounted, ref } from 'vue';
 import Login from '@/components/login/login.vue';
 import { Application } from '@splinetool/runtime';
+import { storeToRefs } from 'pinia';
+import { useMobileStore } from '@/store/mobile';
+
+const { isMobile } = storeToRefs(useMobileStore())
 
 const canvas = ref(null);
 onMounted(() => {
+    if (isMobile.value) return
     const spline = new Application(canvas.value);
     spline.load('https://prod.spline.design/ALUwtc41bPXGlk81/scene.splinecode');
 })
@@ -81,6 +88,17 @@ main {
         .middle_part {
             width: 700px;
             z-index: 100;
+
+            @media screen and (max-width: 768px) {
+                :deep(#login-box) {
+                    transition: all .5s;
+                    transform: translateX(22%);
+
+                    &.right-panel-active {
+                        transform: translateX(-28%);
+                    }
+                }
+            }
         }
     }
 }
