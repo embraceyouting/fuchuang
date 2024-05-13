49d2642565bfa47c68b206e7f180fa6ce9fba94d
TuGitee <tgb@std.uestc.edu.cn>
2024-05-13 23:55:31 +0800

    fix: stream gpt context

--- node/routes/gpt.js
+++ node/routes/gpt.js
@@ -20,7 +20,7 @@ router.get("/stream", async (req, res) => {
 	res.flushHeaders();
 
 	const stream = await openai.beta.chat.completions.stream({
-		messages: [{ role: "user", content: query }],
+		messages: [{ role: "system", content: "你是一个网页体验智能助手，你叫FlyView小助手，你可以分析网页的体验情况，帮助开发者解决网页体验问题，你可以帮助用户直接分析json数据，也可以分析生成的pdf报告，详细说明报告中的内容。当用户输入[help]查看帮助，请回复你的功能。" }, { role: "user", content: query }],
 		// model: "gpt-3.5-turbo",
 		model: "gpt-4-turbo-preview",
 		stream: true,
@@ -44,7 +44,7 @@ router.get("/stream", async (req, res) => {
 router.get("/", async (req, res) => {
 	const query = req.query.key;
 	const result = await openai.chat.completions.create({
-		messages: [{role: "system", content: "你是一个网页体验智能助手，你叫FlyView小助手，你可以分析网页的体验情况，帮助开发者解决网页体验问题，你可以帮助用户直接分析json数据，也可以分析生成的pdf报告，详细说明报告中的内容。当用户输入[help]查看帮助，请回复你的功能。"}, { role: "user", content: query }],
+		messages: [{ role: "system", content: "你是一个网页体验智能助手，你叫FlyView小助手，你可以分析网页的体验情况，帮助开发者解决网页体验问题，你可以帮助用户直接分析json数据，也可以分析生成的pdf报告，详细说明报告中的内容。当用户输入[help]查看帮助，请回复你的功能。" }, { role: "user", content: query }],
 		// model: "gpt-3.5-turbo",
 		model: "gpt-4-turbo-preview",
 	});
