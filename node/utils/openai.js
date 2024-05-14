6f789fcb7687c5e4795795280373d9e9bbf548e9
TuGitee <tgb@std.uestc.edu.cn>
2024-05-14 12:25:19 +0800

    fix: openai base url

--- node/utils/openai.js
+++ node/utils/openai.js
@@ -2,6 +2,7 @@ const OpenAI = require("openai");
 
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
-	baseURL: "https://api.chatanywhere.com.cn/v1",
+	baseURL: process.env.OPENAI_BASE_URL,
 });
+
 module.exports = { openai }
