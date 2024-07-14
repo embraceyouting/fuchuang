b953c4c4f4e683454ee1d23a408a4f8b93fff8bd
TuGitee <tgb@std.uestc.edu.cn>
2024-07-14 23:20:34 +0800

    service timeout

--- serve/src/service/index.js
+++ serve/src/service/index.js
@@ -5,7 +5,7 @@ import { useUserStore } from "@/store/user";
 
 const service = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
-	timeout: 5000,
+	timeout: 30000,
 });
 
 service.interceptors.request.use(
